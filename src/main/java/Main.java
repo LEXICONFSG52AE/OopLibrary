86f22cbf7d3564dc8e70315b3386593fec7eee9b
LEXICONFSG52AE <LEXICONFSG52AE@outlook.com>
2024-10-30 10:46:00 +0100

    1 by the request of teacher. Personal progress: build class for person, building classes for books, having main prepared while building.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+
+        Book book1 = new Book("Test1");
+        Book book2 = new Book("Test2");
+        Book book3 = new Book("Test3");
+
+        Person p1 = new Person("Test Persson");
+        Person p2 = new Person("Per Testsson");
+        System.out.println(p1.getPersonInfo());
+        System.out.println(p2.getPersonInfo());
+
+        System.out.println("----------------------");
+        book1.setOwner(p1);
+        book2.setOwner(p1);
+        book3.setOwner(p1);
+
+        System.out.println(book1.getBookInfo());
+        System.out.println(book2.getBookInfo());
+        System.out.println(book3.getBookInfo());
+
+
+    }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class Person {
+    private static int sequencer = 0;
+
+        private String firstName;
+        private String lastName;
+        private int id;
+
+        public Person(String firstName, String lastName, int id) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = getNextId();
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public String getid() {
+            return id;
+        }
+
+        public String getPersonInfo() {
+            return "Name: " + firstName + ", Lastname: " + lastName; + "ID: " id;
+        }
+
+    // getNext ID ?
+    public static int getNextId(){
+        return ++sequencer;
+
+// Loanbook
+
+
+//returnBook
+
+
+    }
+
+
+
 }
